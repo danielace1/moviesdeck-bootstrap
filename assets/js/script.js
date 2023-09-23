d5b149c0cfead4aa5a1ee40955649bf9231c7240
Sudharsan <danielsudharsan1805@gmail.com>
2023-09-24 02:34:28 +0530

    Final Commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".popup-youtube").magnificPopup({
+    type: "iframe",
+  });
+});
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
